4dbcb1448dd5d4942645dc87daeb97762d79e431
Terrence Tran aka Mr.T <terrencetran@dpengineering.org>
2017-05-10 16:15:55 -0700

    adding button states and pot reading fingers crossed

--- ZOETROBE/ZOETROBE.ino
+++ ZOETROBE/ZOETROBE.ino
@@ -21,9 +21,18 @@
 #define TRANSMISSION 5
 #define BASE_STEPS_PER_REV 200
 
+int beamState = LOW;
+int lastBeamState = LOW;
+int backlightState = LOW;
+int lastBacklightState = LOW;
+int motorState = LOW;
+int lastMotorState = LOW;
+
 FlexyStepper stepper;
 
 void setup() {
+  ////// Pinmode all ports ///////////
+  
   pinMode(MOTOR_BUTTON_LIGHT, OUTPUT);
   pinMode(MOTOR_BUTTON, INPUT_PULLUP);
   pinMode(MOTOR_SPEED_POT, INPUT_PULLUP);
@@ -38,17 +47,66 @@ void setup() {
 
   pinMode(LID_SENSOR, INPUT_PULLUP);
 
+  ////////// Initialize Stepper //////////
+
   stepper.connectToPort(MOTOR_PORT);
   stepper.setStepsPerRevolution(BASE_STEPS_PER_REV * TRANSMISSION * MICROSTEPPING);
   stepper.setSpeedInRevolutionsPerSecond(1.0);
   stepper.setAccelerationInRevolutionsPerSecondPerSecond(5.0);
   stepper.setCurrentPositionInRevolutions(0.0);
-
   stepper.setTargetPositionRelativeInRevolutions(1000.0);
-  stepper.enableStepper();
+  stepper.disableStepper();
+
+  ///////// Initialize Lights and Button Lights //////////
+
+  digitalWrite(MOTOR_BUTTON_LIGHT, motorState);
+  digitalWrite(BEAM_BUTTON_LIGHT, beamState);
+  digitalWrite(BEAM_LEDS, beamState);
+  digitalWrite(BACKLIGHT_BUTTON_LIGHT, backlightState);
+  digitalWrite(BACKLIGHT_LEDS, backlightState);
 
 }
 
 void loop() {
+
+  //////////// Non-Latching Buttons require Button States /////////
+  beamState = digitalRead(BEAM_BUTTON);
+  backlightState = digitalRead(BACKLIGHT_BUTTON);
+  motorState = digitalRead(MOTOR_BUTTON);
+  
+  if (beamState != lastBeamState) {
+    digitalWrite(BEAM_BUTTON_LIGHT, beamState);
+    digitalWrite(BEAM_LEDS, beamState);
+    delay(50);
+  }
+
+  if (backlightState != lastBacklightState) {
+    digitalWrite(BACKLIGHT_BUTTON_LIGHT, backlightState);
+    digitalWrite(BACKLIGHT_LEDS, backlightState);
+    delay(50);
+  }
+
+  if (motorState != lastMotorState) {
+    //On State: enable motor and reset revolution goal
+    if (motorState == HIGH) {
+      digitalWrite(MOTOR_BUTTON_LIGHT, HIGH);
+      stepper.resetVelocity();
+      stepper.setSpeedInRevolutionsPerSecond(analogRead(MOTOR_SPEED_POT)/255.0);
+      stepper.setTargetPositionRelativeInRevolutions(1000.0);
+      stepper.enableStepper();
+    }
+    //Off State: disable motor and turn off light
+    else {
+      digitalWrite(MOTOR_BUTTON_LIGHT, LOW);
+      stepper.disableStepper();
+    }
+    delay(50);
+  }
+  
   stepper.processMovement();
+
+  ///////////// Save Last States For Next Loop Iteration /////////////
+  lastMotorState = motorState;
+  lastBeamState = beamState;
+  lastBacklightState = backlightState;
 }
